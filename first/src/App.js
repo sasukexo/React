ae207e49531b98c235781b019bbaa58be52c4ef2
Sasukexo <aswinash1002@gmail.com>
2023-07-13 19:40:42 +0530

    app

--- first/src/App.js
+++ first/src/App.js
@@ -1,9 +1,15 @@
-import React from 'react'
-
-export const App = () => {
-  return (
-    <div><h1>Hello World</h1></div>
-  )
+function App(){
+  function handleNameChange(){
+    const name=["guru","ram","sai"];
+    const int=Math.floor(Math.random()*3);
+    return name[int];
+  }
+    return(
+        <div>
+          <h1>hELLO</h1>
+            <p>Lets {handleNameChange()}</p>
+        </div>
+    )
 }
 
-export default App
+export default App;
